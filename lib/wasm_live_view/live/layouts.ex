64e0d824523411feea7a43b0485b91eec79dea75
Peter M <petermm@gmail.com>
2026-02-28 12:29:14 +0100

    sticky navbar on mobile

--- lib/wasm_live_view/live/layouts.ex
+++ lib/wasm_live_view/live/layouts.ex
@@ -26,7 +26,7 @@ defmodule WasmLiveView.Layouts do
       <input id="nav-drawer" type="checkbox" class="drawer-toggle" />
       <div class="drawer-content flex flex-col min-h-screen">
         <%!-- Mobile top bar --%>
-        <div class="navbar bg-base-200 border-b border-base-300 lg:hidden">
+        <div class="navbar bg-base-200 border-b border-base-300 lg:hidden sticky top-0 z-50">
           <label for="nav-drawer" class="btn btn-ghost btn-square drawer-button">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-5 w-5 stroke-current">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
