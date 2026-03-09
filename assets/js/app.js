a51b5acde825224801905ca781ea5c4cf7463e72
Peter M <petermm@gmail.com>
2026-03-09 19:20:46 +0100

    fix geolocation issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -316,10 +316,11 @@ Hooks.EaselCanvas = {
   },
 };
 
-// Geolocation hook: gets browser geolocation when triggered
+// Geolocation hook: gets browser geolocation when triggered.
+// Uses a single shared instance so multiple LiveViews don't conflict.
 Hooks.Geolocation = {
   mounted() {
-    this.handleEvent("check-permission", () => {
+    this._checkPermission = () => {
       if (!navigator.permissions) return;
       navigator.permissions
         .query({ name: "geolocation" })
@@ -329,9 +330,9 @@ Hooks.Geolocation = {
           }
         })
         .catch(() => {});
-    });
+    };
 
-    this.handleEvent("request-location", () => {
+    this._requestLocation = () => {
       if (!navigator.geolocation) {
         this.pushEvent("location-error", {
           error: "Geolocation not supported",
@@ -349,9 +350,12 @@ Hooks.Geolocation = {
         (err) => {
           this.pushEvent("location-error", { error: err.message });
         },
-        { enableHighAccuracy: true, timeout: 10000 },
+        { timeout: 15000, maximumAge: 300000 },
       );
-    });
+    };
+
+    this.handleEvent("check-permission", this._checkPermission);
+    this.handleEvent("request-location", this._requestLocation);
   },
 };
 
